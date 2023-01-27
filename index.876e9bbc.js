5feaa3f3edfad9a4c20cc602a13b0a22070f6f43
KlaudiaKrekora <116798920+KlaudiaKrekora@users.noreply.github.com>
2023-01-27 12:50:50 +0000

    Deploying to gh-pages from @ KlaudiaKrekora/team-project-IceCream@4ce2b3420dbf433e0bfbcde91bb54d5fd489c3ab 🚀

---
+++ index.876e9bbc.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.876e9bbc.js.map
