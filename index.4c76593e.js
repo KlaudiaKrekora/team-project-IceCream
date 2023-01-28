c4ca4721ebae42944bed68d4144dae67231e5eae
KlaudiaKrekora <116798920+KlaudiaKrekora@users.noreply.github.com>
2023-01-28 14:30:30 +0000

    Deploying to gh-pages from @ KlaudiaKrekora/team-project-IceCream@4ff3c958d20ffd89ea8329485dabdb90227f0327 🚀

---
+++ index.4c76593e.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{autoplay:{delay:4e3},speed:1e3,loop:!0,pagination:{el:".swiper-pagination"}});
+//# sourceMappingURL=index.4c76593e.js.map
--- index.9174d0b3.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=index.9174d0b3.js.map
