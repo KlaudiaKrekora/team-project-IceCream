6f7ecc97ec8902c3e6e0621a05999e4808f9916f
Justyna <justyna.sienkiewicz93@gmail.com>
2023-02-02 10:52:33 +0100

    removed cdn for scroll lock

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -52,6 +52,5 @@
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
