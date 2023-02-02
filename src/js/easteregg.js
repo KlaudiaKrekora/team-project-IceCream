b9b7ec0ffbb5c4dcad5015b23873e40db5de4876
kgryguc <krzysztofgryguc@gmail.com>
2023-02-02 14:17:27 +0100

    fix easteregg size, link area, names positioning

--- src/js/easteregg.js
+++ src/js/easteregg.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById('easteregg').style.height = '320px';
+  document.getElementById('easteregg').style.height = '250px';
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
