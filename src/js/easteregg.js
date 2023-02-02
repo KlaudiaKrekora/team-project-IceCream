247509186074ae1ee58b501bc045c4376a094e2f
kgryguc <krzysztofgryguc@gmail.com>
2023-02-02 04:19:09 +0100

    easteregg version 1 complete

---
+++ src/js/easteregg.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById('easteregg').style.height = '320px';
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById('easteregg').style.height = '0';
+}
