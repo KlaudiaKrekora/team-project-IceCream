8e6adb438deb6207d90b215a7527bceb4d7b4d98
kgryguc <krzysztofgryguc@gmail.com>
2023-02-02 17:06:59 +0100

    remove uneccesary code

--- src/js/easteregg.js
+++ src/js/easteregg.js
@@ -1,9 +1,7 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById('easteregg').style.height = '250px';
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById('easteregg').style.height = '0';
 }
