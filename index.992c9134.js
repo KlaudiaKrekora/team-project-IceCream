aeb3720047fdec8700f5eb7ce456d626e2111bc2
kgryguc <63057362+kgryguc@users.noreply.github.com>
2023-02-02 17:02:38 +0000

    Deploying to gh-pages from @ KlaudiaKrekora/team-project-IceCream@f4f47374e47415473065fcedc7082093d368a1ed 🚀

---
+++ index.992c9134.js
@@ -0,0 +1,2 @@
+function openNav(){document.getElementById("easteregg").style.height="250px"}function closeNav(){document.getElementById("easteregg").style.height="0"}
+//# sourceMappingURL=index.992c9134.js.map
