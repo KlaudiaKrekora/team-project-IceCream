f41f3f18287f49fb93410542ed7e126bd1c5365b
kgryguc <krzysztofgryguc@gmail.com>
2023-02-02 02:13:56 +0100

    add close mobile menu by clicking backdrop option, add blur to backdrop, add opacity do menu, close menu on buy button click

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,29 +1,50 @@
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
+  const mobileMenuBox = document.querySelector('.menu-backdrop');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
   const buyButton = document.querySelector('.header-buttons__cta');
+  const buyButtonMobile = document.querySelector('.button--menu');
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   function hideButton() {
     buyButton.classList.toggle('is-hidden');
   }
 
+  function hideBackdrop() {
+    mobileMenuBox.classList.toggle('is-hidden-menu');
+  }
+
   openMenuBtn.addEventListener('click', () => {
     toggleMenu();
     hideButton();
+    hideBackdrop();
   });
   closeMenuBtn.addEventListener('click', () => {
     toggleMenu();
     hideButton();
+    hideBackdrop();
+  });
+
+  mobileMenuBox.addEventListener('click', () => {
+    toggleMenu();
+    hideButton();
+    hideBackdrop();
+  });
+
+  buyButtonMobile.addEventListener('click', () => {
+    toggleMenu();
+    hideButton();
+    hideBackdrop();
+  });
+
+  mobileMenu.addEventListener('click', e => {
+    e.stopPropagation();
   });
 
   // Close the mobile menu on wider screens if the device orientation changes
